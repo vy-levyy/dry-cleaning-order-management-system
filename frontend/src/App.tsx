42bc74f0534a8a8def547c063d60dde589a8662a
Vyacheslav Levyy <volllon2.0@yandex.ru>
2020-05-14 14:21:45 +0300

    Added router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 import store from './store';
 import { Provider } from 'react-redux';
 import PageRegistration from './pages/PageRegistration';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 const App: React.FunctionComponent = () => {
   return (
     <Provider store={ store }>
-      <PageRegistration />
+      <Router>
+          <Switch>
+            <Route path="/registration" exact>
+              <PageRegistration />
+            </Route>
+          </Switch>
+        </Router>
     </Provider>
   );
 }
